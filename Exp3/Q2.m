5ff47d3d83da3b531162ecbc22ccc410d32048c4
Joseph Joy <josephelnova18@gmail.com>
2024-09-26 18:01:54 +0530

    Linear Convolution

--- Exp3/Q2.m
+++ Exp3/Q2.m
@@ -1,18 +1,26 @@
-% Linear Convolution using inbuilt function
-clc;
-clear;
-close all;
+%Linear convolution without using inbuilt functions
 % Input sequences and their indices
 x = input('Enter input sequence x: ');
 x_ind = input('Enter index of x: ');
 h = input('Enter impulse response h: ');
 h_ind = input('Enter index of h: ');
-% Linear convolution
-y = conv(x, h);
+% Get the length of the sequences
+len_x = length(x);
+len_h = length(h);
+% Calculate the length of the convolution result
+len_y = len_x + len_h - 1;
+% Initialize the result sequence with zeros
+y = zeros(1, len_y);
+% Perform the convolution
+for i = 1:len_x
+for j = 1:len_h
+y(i + j - 1) = y(i + j - 1) + x(i) * h(j);
+end
+end
 % Determine the time indices for the convolution result
 y_ind = min(x_ind) + min(h_ind) : max(x_ind) + max(h_ind);
-% Display the convolution result
-disp('Convolution result y:');
+% Display the result
+disp('Linear Convolution Result:');
 disp(y);
 % Plotting the input sequences and the convolution result
 % Create a figure window
